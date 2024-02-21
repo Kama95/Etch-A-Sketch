45f82ac82e3b3f7dfeaeb08f36ce6069f59cd90c
Kama <kamasimbeye89@gmail.com>
2024-02-21 12:24:37 +0200

    Create function to multiply the squareboxes on the page

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 
-const squareBox = document.createElement('div');
+//const squareBox = document.createElement('div');
+//squareBox.classList.add('square')
+//const containerEle=document.querySelector('.container');
+//containerEle.appendChild(squareBox);
+
+
+function increaseSquares (num){
+  for (i=0; i < num;i++){
+    const squareBox = document.createElement('div');
 squareBox.classList.add('square')
 const containerEle=document.querySelector('.container');
-containerEle.appendChild(squareBox);
-
-
-//function increaseSquares (num){
-  //for (i=0; i < num;i++)
-
-
+    containerEle.appendChild(squareBox);
+  }
 }
+
+increaseSquares(10);
