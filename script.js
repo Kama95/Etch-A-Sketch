fc4c7606e5f53f85a66632f9fd9a0af2ddac9617
Kama <kamasimbeye89@gmail.com>
2024-02-21 12:17:54 +0200

    Create Web Page link CSS and Javascript to html

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const squareBox = document.createElement('div');
+squareBox.classList.add('square')
+const containerEle=document.querySelector('.container');
+containerEle.appendChild(squareBox);
+
+
+function increaseSquares (num){
+  for (i=0; i < num;i++)
+
+}
