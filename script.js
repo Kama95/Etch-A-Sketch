3739288985ae0b4eed1f474d12d7bce21b94319d
Kama <kamasimbeye89@gmail.com>
2024-02-21 12:44:07 +0200

    Modify function to create rows and columns, instead of just 1 row

--- script.js
+++ script.js
@@ -5,13 +5,16 @@
 //containerEle.appendChild(squareBox);
 
 
-function increaseSquares (num){
-  for (i=0; i < num;i++){
+function increaseSquares (row,col){
+  for (let i=0; i < row;i++){
+    for (let j=0;j<col;j++){
+
     const squareBox = document.createElement('div');
 squareBox.classList.add('square')
 const containerEle=document.querySelector('.container');
     containerEle.appendChild(squareBox);
   }
 }
+}
 
-increaseSquares(10);
+increaseSquares(50,50);
