8d2439907d8947dac0f2a4171b4bc3fb346545ff
Kama <kamasimbeye89@gmail.com>
2024-02-23 13:03:47 +0200

    rework function to create an equal number of cilumns and rows.

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
 
-//const squareBox = document.createElement('div');
-//squareBox.classList.add('square')
-//const containerEle=document.querySelector('.container');
-//containerEle.appendChild(squareBox);
-
-
-//function increaseSquares (row,col){
-  //for (let i=0; i < row;i++){
-    //for (let j=0;j<col;j++){
-
-    //const squareBox = document.createElement('div');
-//squareBox.classList.add('square')
-//const containerEle=document.querySelector('.container');
-  //  containerEle.appendChild(squareBox);
-  ///}
-//}/
-//}
-
-//increaseSquares(16,16);
-
 function createGrid(size){
   const sketchSpace = document.querySelector ('.sketchScreen');
   for (let i=0;i<size;i++){
