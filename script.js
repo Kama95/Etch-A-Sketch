749bc2dd980e882938882e9d973ce376bf7d9eda
Kama <kamasimbeye89@gmail.com>
2024-02-23 13:02:54 +0200

    Add more divs to html

--- script.js
+++ script.js
@@ -5,16 +5,33 @@
 //containerEle.appendChild(squareBox);
 
 
-function increaseSquares (row,col){
-  for (let i=0; i < row;i++){
-    for (let j=0;j<col;j++){
+//function increaseSquares (row,col){
+  //for (let i=0; i < row;i++){
+    //for (let j=0;j<col;j++){
 
-    const squareBox = document.createElement('div');
-squareBox.classList.add('square')
-const containerEle=document.querySelector('.container');
-    containerEle.appendChild(squareBox);
+    //const squareBox = document.createElement('div');
+//squareBox.classList.add('square')
+//const containerEle=document.querySelector('.container');
+  //  containerEle.appendChild(squareBox);
+  ///}
+//}/
+//}
+
+//increaseSquares(16,16);
+
+function createGrid(size){
+  const sketchSpace = document.querySelector ('.sketchScreen');
+  for (let i=0;i<size;i++){
+    const column = document.createElement('div');
+    column.classList.add('column');
+  for (let j=0 ;j < size; j++){
+    let row = document.createElement('div');
+    row.classList.add('row');
+  
+    column.appendChild(row)
+  }
+
+  sketchSpace.appendChild(column)
   }
 }
-}
-
-increaseSquares(50,50);
+createGrid(5)
